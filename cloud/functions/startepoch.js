feaf9c4bb92ec7c29fa27525559662dc234ef7b0
okerye <7309385+okerye@users.noreply.github.com>
2022-06-10 09:47:29 +0800

    Create startepoch.js

---
+++ cloud/functions/startepoch.js
@@ -0,0 +1,13 @@
+//start a new epoch
+//1. archive old epoch(if have)
+//2. create epoch data
+//3. create puzzle objects
+//4. create unsolved puzzle list
+//5. create solved puzzle list
+//6. create solving puzzle list
+//7. create solved-count player rank list
+//8. create hard-puzzle rank list
+Parse.Cloud.define("startepoch", ()=>
+{
+	console.log("startepoch called!");
+});
