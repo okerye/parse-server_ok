1112d7fae856c36d5b15fb61f9ce197762b7a5a1
okerye <7309385+okerye@users.noreply.github.com>
2022-06-10 11:53:14 +0800

    Update startepoch.js

--- cloud/functions/startepoch.js
+++ cloud/functions/startepoch.js
@@ -7,8 +7,26 @@
 //6. create solving puzzle list
 //7. create solved-count player rank list
 //8. create hard-puzzle rank list
+
+var EpochData = Parse.Object.extend("EpochData");
+var testepochdata = {
+	"epochcode": "e202201",
+	"endtime": ""
+};
+
 Parse.Cloud.define('startepoch', ()=>
 {
 	//console.log("startepoch called!");
+	//var promise = new Parse.Promise();
+	//var promises = [];
+	var epochdata = new EpochData();
+	epochdata.save(testepochdata).then(function() {
+		return 'startepoch succeeded!';
+	});
+	//promises.push(epochdata.save(testepochdata));
+
+	//Parse.Promise.when(promises).then(function() {
+	//	promise.resolve();
+	//});
 	return 'startepoch called!';
 });
