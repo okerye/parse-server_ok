81261823594802a1956f7c6d76ec412e5aa9ee45
okerye <7309385+okerye@users.noreply.github.com>
2022-06-10 13:32:21 +0800

    test cloud code

--- cloud/functions/startepoch.js
+++ cloud/functions/startepoch.js
@@ -30,3 +30,14 @@ Parse.Cloud.define('startepoch', ()=>
 	//});
 	return 'startepoch called!';
 });
+
+Parse.Cloud.define("averageStars", async (request) => {
+  const query = new Parse.Query("Review");
+  query.equalTo("movie", request.params.movie);
+  const results = await query.find();
+  let sum = 0;
+  for (let i = 0; i < results.length; ++i) {
+    sum += results[i].get("stars");
+  }
+  return sum / results.length;
+});
