c3a2e1458e665e7c58a513a163e07b1a418aebcb
zhangcong <7309385+okerye@users.noreply.github.com>
2022-06-20 22:32:32 +0800

    Update getpuzzle.js

--- cloud/functions/getpuzzle.js
+++ cloud/functions/getpuzzle.js
@@ -1,12 +1,24 @@
 var LevelData = Parse.Object.extend("LevelData");
+var EpochData = Parse.Object.extend("EpochData");
 var UnsolvedPuzzleList = Parse.Object.extend("UnsolvedPuzzleList");
+var SolvedPuzzleList = Parse.Object.extend("SolvedPuzzleList");
+var SolvingPuzzleList = Parse.Object.extend("SolvingPuzzleList");
 Parse.Cloud.define("getpuzzle", async(requestpara) => {
-	const query = new Parse.Query(LevelData);
+	const querylevel = new Parse.Query(LevelData);
+	const queryepochdata = new Parse.Query(EpochData);
+	const queryplayer = new Parse.Query(Parse.User);
 	var playerid = requestpara.user.id;
 	var puzzletype = requestpara.params.para1;
 	var puzzleid = requestpara.params.para2;
 	var playerepochid = requestpara.params.para3;
 	console.log("Player Id: " + playerid);
+
+	const player = await queryplayer.get(playerid);
+	const epochcode = await player.get(playerEpochId);
+
+	console.log("epoch Id: " + epochcode);
+
+
 	if(puzzletype == "Challenge")
 	{
 		const querypuzzlelist = new Parse.Query(UnsolvedPuzzleList);
@@ -14,12 +26,13 @@ Parse.Cloud.define("getpuzzle", async(requestpara) => {
 	} 
 	else if(puzzletype == "Practice")
 	{
-
+		const querypuzzlelist = new Parse.Query(SolvedPuzzleList);
 	}
 	else if(puzzleid != "")
 	{
-
+		const querypuzzle = new Parse.Query(LevelData);
 	}
+
 	// await query.first().then((seed)=>{
 	// 	if(seed === undefined)
 	// 	{
