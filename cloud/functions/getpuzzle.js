1864f817d61c8f077a538ca644ee3b4eeec8b661
zhangcong <7309385+okerye@users.noreply.github.com>
2022-06-21 23:26:09 +0800

    Update getpuzzle.js

--- cloud/functions/getpuzzle.js
+++ cloud/functions/getpuzzle.js
@@ -108,7 +108,7 @@ async function generateplayrecord(puzzleid, playerid)
 async function updatepuzzledata(puzzleid)
 {
 	const querylevel = new Parse.Query(LevelData);
-	const puzzledataobj = await querylevel.get(puzzleid);
+	const puzzledataobj = puzzleid;//await querylevel.get(puzzleid);
 	puzzledataobj.increment("playedtimes");
 	puzzledataobj.save();
 }
