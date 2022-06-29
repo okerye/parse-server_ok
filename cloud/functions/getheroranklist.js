3d6ade08b80848a05dc0bf7b06114f6be3b5d519
okerye <7309385+okerye@users.noreply.github.com>
2022-06-29 14:14:07 +0800

    增加获取英雄榜

---
+++ cloud/functions/getheroranklist.js
@@ -0,0 +1,27 @@
+var HeroRankList = Parse.Object.extend("HeroRankList");
+
+Parse.Cloud.define("getheroranklist", async(requestpara) => {
+	const queryheroranklist = new Parse.Query(HeroRankList);
+	const queryplayer = new Parse.Query(Parse.User);
+	var playerid = requestpara.user.id;
+	const player = await queryplayer.get(playerid);
+	const epochcode = await player.get("playerEpochId");
+	queryheroranklist.ascending("challengesuccess");
+	var results = [];
+	var heroranklist = await queryheroranklist.find();
+	for(let i = 0; i < heroranklist.length; i++){
+		const obj = heroranklist[i];
+		await obj.fetch();
+		const heroid = await obj.get("playerid");
+		const hero = await queryplayer.get(heroid);
+		const heroepochcode = await hero.get("playerEpochId");
+		if(epochcode != heroepochcode)
+			continue;
+		var resultjson = {};
+		resultjson.nickname = await obj.get("nickname");
+		resultjson.challengesuccess = await obj.get("challengesuccess");
+		resultjson.challengetimes = await obj.get("challengetimes");
+		results.push(resultjson);
+	}
+	return results;
+});
--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,4 @@ require("./functions/sign.js");
 require("./functions/getpuzzle.js");
 require("./functions/verifypuzzle.js");
 require("./functions/gethardpuzzleranklist.js");
+require("./functions/getheroranklist.js");
