52ffc78e3ea118dfeb93151e25aa53ae498cb441
okerye <7309385+okerye@users.noreply.github.com>
2022-07-28 15:21:51 +0800

    Update getpuzzle.js

--- cloud/functions/getpuzzle.js
+++ cloud/functions/getpuzzle.js
@@ -54,7 +54,8 @@ Parse.Cloud.define("getpuzzle", async(requestpara) => {
 		var idlepuzzle = false;
 		var counter = 0;
 		console.log("playerpuzzleids: " + playerpuzzleids);
-		querylevel.notContainedIn('objectId',playerpuzzleids);
+		if(playerpuzzleids.length > 0)
+			querylevel.notContainedIn('objectId',playerpuzzleids);
 		const now = new Date();
 		querylevel.lessThan("endtime", now);
 		puzzleidchosen = await querylevel.first();
